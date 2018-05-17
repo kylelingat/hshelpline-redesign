860c1ef4c7167628ef4981c257daf4de1ef2e5aa
kylelingat <kylejlingat@gmail.com>
2018-05-16 23:44:09 -1000

    Image linking fix

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 document.getElementById('home').addEventListener("click", function() {
-    $('#imgFront').css('background-image', 'url(../images/hero2.jpg)');
+    $('#imgFront').css('background-image', 'url(./images/hero2.jpg)');
     $('#boxContainer').css('display', 'block');
 });
 
 
 document.getElementById('careers').addEventListener("click", function() {
-    $('#imgFront').css('background-image', 'url(../images/hero3.jpg)');
+    $('#imgFront').css('background-image', 'url(./images/hero3.jpg)');
     $('#imgFront').css('transition', 'background-image 0.2s ease-in-out');
     $(".menuItem").hover(function() {
         $(this).css("color", "orange")
@@ -25,16 +25,16 @@ document.getElementById('careers').addEventListener("click", function() {
 });
 
 document.getElementById('contact').addEventListener("click", function() {
-    $('#imgFront').css('background-image', 'url(../images/hero4.jpeg)');
+    $('#imgFront').css('background-image', 'url(./images/hero4.jpeg)');
     $('#boxContainer').css('display', 'none');
 });
 
 document.getElementById('faq').addEventListener("click", function() {
-    $('#imgFront').css('background-image', 'url(../images/hero5.jpg)');
+    $('#imgFront').css('background-image', 'url(./images/hero5.jpg)');
     $('#boxContainer').css('display', 'none');
 });
 
 document.getElementById('stats').addEventListener("click", function() {
-    $('#imgFront').css('background-image', 'url(../images/hero6.jpg)');
+    $('#imgFront').css('background-image', 'url(./images/hero6.jpg)');
     $('#boxContainer').css('display', 'none');
 });
