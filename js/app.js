efd27395ad4704a32c65f75d50743f899a67d12c
kylelingat <kylejlingat@gmail.com>
2018-05-20 18:01:19 -1000

    Preload images

--- js/app.js
+++ js/app.js
@@ -1,10 +1,73 @@
+var images = [];
+function preload() {
+    for (var i = 0; i < arguments.length; i++) {
+        images[i] = new Image();
+        images[i].src = preload.arguments[i];
+    }
+}
+preload(
+    "./images/hero3.jpg",
+    "./images/hero4.jpg",
+    "./images/hero5.jpg",
+    "./images/hero6.jpg",
+    "./images/helpline.jpg",
+    "./images/waipahu.jpg",
+    "./images/nami.jpg"
+)
 $(".menuItem").hover(function() {
     $(this).css({"color":"#90C695","font-size":"33px"});
 }).mouseout(function() {
     $(this).css({"color":"white","font-size":"30px"});
 });
 
+var onHome = 1;
+var onCareer = 0;
+var onContact = 0;
+var onFaq = 0;
+var onStat = 0;
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '37') {
+      if (onHome == 1) {
+        statsClick()
+      } else if (onCareer == 1) {
+        homeClick()
+      } else if (onContact == 1) {
+        careersClick()
+      } else if (onFaq == 1) {
+        contactClick();
+      } else if(onStat == 1) {
+        faqClick();
+      }
+    }
+
+    else if (e.keyCode == '39') {
+      if (onHome == 1) {
+        careersClick()
+      } else if (onCareer == 1) {
+        contactClick()
+      } else if (onContact == 1) {
+        faqClick()
+      } else if (onFaq == 1) {
+        statsClick();
+      } else if(onStat == 1) {
+        homeClick();
+      }
+    }
+
+}
+
 function homeClick() {
+    onHome = 1;
+    onCareer = 0;
+    onContact = 0;
+    onFaq = 0;
+    onStat = 0;
     var $target = $('html,body');
     $target.animate({scrollTop: $("#navBar").offset().top}, 700);
     $('#imgFront').css('background-image', 'url(./images/hero2.jpg)');
@@ -27,6 +90,11 @@ function homeClick() {
 
 
 function careersClick() {
+    onHome = 0;
+    onCareer = 1;
+    onContact = 0;
+    onFaq = 0;
+    onStat = 0;
     var $target = $('html,body');
     $target.animate({scrollTop: $("#navBar").offset().top}, 700);
     $('#imgFront').css('background-image', 'url(./images/hero3.jpg)');
@@ -39,6 +107,7 @@ function careersClick() {
     $('#boxContainer').css('display', 'none');
     $('#statsContainer').css('display', 'none');
     $('#contactContainer').css('display', 'none');
+    $('.menuItem').not('#careers').css('color', 'white');
     $(".menuItem").hover(function() {
         $(this).css({"color":" #1F3A93","font-size":"33px"});
     }).mouseout(function() {
@@ -54,9 +123,14 @@ function careersClick() {
 }
 
 function contactClick() {
+    onHome = 0;
+    onCareer = 0;
+    onContact = 1;
+    onFaq = 0;
+    onStat = 0;
     var $target = $('html,body');
     $target.animate({scrollTop: $("#navBar").offset().top}, 700);
-    $('#imgFront').css('background-image', 'url(./images/hero4.jpeg)');
+    $('#imgFront').css('background-image', 'url(./images/hero4.jpg)');
     $('#contact').css('color', '#F64747');
     $('#contact').css('border-bottom', '5px solid #F64747');
     $('#careers').css('border-bottom', 'none');
@@ -67,6 +141,7 @@ function contactClick() {
     $('#faqContainer').css('display', 'none');
     $('#statsContainer').css('display', 'none');
     $('#careersContainer').css('display', 'none');
+    $('.menuItem').not('#contact').css('color', 'white');
     $(".menuItem").hover(function() {
         $(this).css({"color":"#F64747","font-size":"33px"});
     }).mouseout(function() {
@@ -75,6 +150,11 @@ function contactClick() {
 }
 
 function faqClick() {
+    onHome = 0;
+    onCareer = 0;
+    onContact = 0;
+    onFaq = 1;
+    onStat = 0;
     var $target = $('html,body');
     $target.animate({scrollTop: $("#navBar").offset().top}, 700);
     $('#imgFront').css('background-image', 'url(./images/hero5.jpg)');
@@ -87,6 +167,7 @@ function faqClick() {
     $('#boxContainer').css('display', 'none');
     $('#careersContainer').css('display', 'none');
     $('#statsContainer').css('display', 'none');
+    $('.menuItem').not('#faq').css('color', 'white');
     $(".menuItem").hover(function() {
         $(this).css({"color":"#5ec9ca","font-size":"33px"});
     }).mouseout(function() {
@@ -96,6 +177,11 @@ function faqClick() {
 }
 
 function statsClick() {
+    onHome = 0;
+    onCareer = 0;
+    onContact = 0;
+    onFaq = 0;
+    onStat = 1;
     var $target = $('html,body');
     $target.animate({scrollTop: $("#navBar").offset().top}, 700);
     $('#imgFront').css('background-image', 'url(./images/hero6.jpg)');
@@ -109,6 +195,7 @@ function statsClick() {
     $('#careersContainer').css('display', 'none');
     $('#contactContainer').css('display', 'none');
     $('#statsContainer').css('display', 'block');
+    $('.menuItem').not('#stats').css('color', 'white');
     $(".menuItem").hover(function() {
         $(this).css({"color":"#663399","font-size":"33px"});
     }).mouseout(function() {
@@ -136,10 +223,19 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-
 $(document).ready(function(){
     $(".disclaimer").mouseover(function(){
+      if(onHome == 1){
         $(this).css("background-color", "#90C695");
+      } else if (onCareer == 1) {
+        $(this).css("background-color", "#F64747");
+      } else if (onContact == 1) {
+        $(this).css("background-color", "#1F3A93");
+      } else if (onFaq == 1) {
+        $(this).css("background-color", "#5ec9ca");
+      } else if (onStat == 1) {
+        $(this).css("background-color", "#663399");
+      }
         $(".disclaimer p").css("display", "block");
         $(".disclaimer h1").css("color", "white");
         $(".disclaimer p").css("color", "white");
